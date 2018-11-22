6d1dd3adaf7b068b805b9515fdbe5fe5824917b8
joshgrant <joshua.m.grant@gmail.com>
2018-11-22 15:06:59 -0500

    multiple tests

--- test_sample.py
+++ test_sample.py
@@ -16,3 +16,25 @@ def test_basic(selenium):
     actual = selenium.title
 
     assert expected == actual
+
+def test_valid_login(selenium):
+    selenium.get('https://www.saucedemo.com')
+
+    selenium.find_element_by_css_selector('[data-test="username"]').send_keys('standard_user')
+    selenium.find_element_by_css_selector('[data-test="password"]').send_keys('secret_sauce')
+    selenium.find_element_by_css_selector('.login-button').click()
+
+    expected = 'Swag Labs'
+    actual = selenium.find_element_by_id('header_container').text
+    
+    assert expected == actual
+
+def test_invalid_login(selenium):
+    selenium.get('https://www.saucedemo.com')
+
+    selenium.find_element_by_css_selector('[data-test="username"]').send_keys('bad_user')
+    selenium.find_element_by_css_selector('[data-test="password"]').send_keys('bad_pass')
+    selenium.find_element_by_css_selector('.login-button').click()
+
+    assert selenium.find_element_by_css_selector('.error-button').is_displayed()
+
